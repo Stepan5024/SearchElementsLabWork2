3b408e34bcbf5318148080e11efb83f41b6aaa7c
Степан <bokarevstepan1@yandex.ru>
2021-09-26 15:03:29 +0300

    Добавьте файлы проекта.

---
+++ LabWork2SearchElements/LabWork2SearchElements.cpp
@@ -0,0 +1,11 @@
+﻿// Лабораторная работа №2 
+// Выполнили студенты группы м3о-219Бк-20 Бокарев, Катвалян
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
